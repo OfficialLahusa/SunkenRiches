92c38722ea1e2ae0cee786ef1701f20029bddab2
Lasse Huber-Saffer <lasse@huber-saffer.de>
2025-10-25 19:20:49 +0200

    Initial postprocessing

--- Unity/Sunken Riches/Assets/Scripts/Timer.cs
+++ Unity/Sunken Riches/Assets/Scripts/Timer.cs
@@ -1,9 +1,13 @@
+using Rive;
+using Rive.Components;
 using System;
 using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    public RiveWidget timerWidget;
+
     TMP_Text text;
 
     // Set timer to 3 min
@@ -30,5 +34,53 @@ public class Timer : MonoBehaviour
         int seconds = Mathf.FloorToInt(Mathf.Repeat(remainingSeconds, 60f));
 
         text.SetText($"{minutes}:{seconds:D2}");
+    }
+
+    private void OnEnable()
+    {
+        timerWidget.OnWidgetStatusChanged += HandleWidgetStatusChanged;
+    }
+
+    private void OnDisable()
+    {
+        timerWidget.OnWidgetStatusChanged -= HandleWidgetStatusChanged;
+    }
+
+    private void HandleWidgetStatusChanged()
+    {
+        if (timerWidget.Status == WidgetStatus.Loaded)
+        {
+            File file = timerWidget.File;
+
+            /*// Get reference by name
+            ViewModel viewModel = file.GetViewModelByName("My View Model");
+
+            // Get reference by index
+            for (int i = 0; i < file.ViewModelCount; i++)
+            {
+                ViewModel indexedVM = file.GetViewModelAtIndex(i);
+            }*/
+
+            // Get reference to the default view model for an artboard
+            ViewModel defaultVM = timerWidget.Artboard.DefaultViewModel;
+            //ViewModel defaultVM = timerWidget.File.
+
+            ViewModelInstance defaultVMI = defaultVM.CreateDefaultInstance();
+
+            timerWidget.StateMachine.BindViewModelInstance(defaultVMI);
+
+            // A list of properties
+            var properties = defaultVM.Properties;
+            foreach (var prop in properties)
+            {
+                Debug.Log($"Property: {prop.Name}, Type: {prop.Type}");
+            }
+
+            foreach(SMIInput input in timerWidget.StateMachine.Inputs())
+            {
+                Debug.Log(input.Name);
+                Debug.Log(input.IsNumber);
+            }
+        }
     }
 }
