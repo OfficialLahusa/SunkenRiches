0db0f776432a58408dbc433cb04102e47217f043
Lasse Huber-Saffer <lasse@huber-saffer.de>
2025-10-26 15:28:31 +0100

    Score display

---
+++ Unity/Sunken Riches/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    TMP_Text text;
+
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+
+    void OnAwake()
+    {
+        int totalScore = 30 * ScoreTracker.Instance.trashCollected + 100 * ScoreTracker.Instance.commonCollected + 250 * ScoreTracker.Instance.rareCollected;
+
+        text.SetText($"Trash collected: {ScoreTracker.Instance.trashCollected}\n"
+            + $"Common collected: {ScoreTracker.Instance.commonCollected}\n"
+            + $"Rare collected: {ScoreTracker.Instance.rareCollected}\n\n"
+            + $"Total score: {totalScore}");
+    }
+}
