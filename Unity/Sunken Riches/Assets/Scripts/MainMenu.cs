aa1fd077ff15992b6390a9ab5d48de6b82832cee
Lasse Huber-Saffer <lasse@huber-saffer.de>
2025-10-26 17:34:11 +0100

    Unlock cursor in UI scenes

--- Unity/Sunken Riches/Assets/Scripts/MainMenu.cs
+++ Unity/Sunken Riches/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Start()
+    {
+        // Unlock cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("Main", LoadSceneMode.Single);
